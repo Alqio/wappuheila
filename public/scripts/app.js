4dfa13953b17979044f93f119555dd8ac461886f
Joonas Ulmanen <joonas.ulmanen@gmail.com>
2016-03-08 01:42:03 +0200

    Remove unused and duplicate code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,5 +7,16 @@ angular.module('wappuheila', [])
     this.registration = {};
     var self = this;
 
-
+    this.postData = function(){
+        console.log("Trying to sign up\n");
+        
+        $http.post("/registration", self.registration)
+            .then(function(response){
+            }, function(response,err){
+                console.log("error\n");
+                self.registrationStatus = true;
+                self.registrationMessage = "VIRHE: " + response.data.msg;
+                console.log(self.registrationMessage);
+            }); 
+    };
 });
